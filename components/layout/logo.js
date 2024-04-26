9ade98fe9683cf223b978bc1f6388cc409187d52
AlbertREtouch <499391071@qq.com>
2024-04-25 23:54:33 -0700

    fix logo state

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -2,19 +2,31 @@ import { useRecipes } from "@/store/recipe-context";
 import Image from "next/image";
 import Link from "next/link";
 
+const Logo = () => {
+  const { setIsGetRandom, setSelectedCategory } = useRecipes();
+  const handleClick = () => {
+    setSelectedCategory(null);
+    setIsGetRandom(true);
+  };
 
-const  Logo= () => {
-    const { setIsGetRandom } = useRecipes();
-    const handleClick = () => {
-        setIsGetRandom(true);
-      };
+  return (
+    <div
+      className="flex flex-row items-center justify-between gap-2"
+      onClick={handleClick}
+    >
+      <Image
+        href="/"
+        alt="Logo"
+        className="hidden md:block cursor-pointer"
+        height="40"
+        width="40"
+        src="/logo.svg"
+      />
+      <Link href="/" className="font-bold">
+        Dish Delights
+      </Link>
+    </div>
+  );
+};
 
-    return ( 
-        <div className="flex flex-row items-center justify-between gap-2" onClick={handleClick}>
-        <Image href= "/" alt="Logo" className="hidden md:block cursor-pointer" height="40" width="40" src="/logo.svg" /> 
-        <Link href="/" className="font-bold">Dish Delights</Link>
-        </div>        
-     );
-}
- 
 export default Logo;
